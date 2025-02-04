18ea3ff35202d71fa5e1425eaadcf7d72efa11b4
Tarnumx <simmons@yandex.ru>
2025-02-04 13:02:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+var md = markdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+  highlight,
+})
+  .use(require('markdown-it-sub'))
+  .use(require('markdown-it-footnote'))
+  .use(require('markdown-it-deflist'))
+  .use(require('markdown-it-abbr'))
+  .use(require('markdown-it-attrs'))
+  .use(require('markdown-it-anchor')
